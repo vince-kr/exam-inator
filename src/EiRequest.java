6f088014a85c7f1e8bf17846590d31537ec7e68c
Vince <vince@krijnsen.com>
2023-10-16 20:12:03 +0100

    Extend EiRequest class before extracting super
    Add one more constructor to EiRequest (now 3 total) plus a Parent field
    to enable moving backwards in the menu. The EiRequest class is getting
    too big and I will create an abstract class to inherit types of request
    from.

--- src/EiRequest.java
+++ src/EiRequest.java
@@ -1,12 +1,26 @@
 import java.util.HashMap;
 import java.util.regex.Pattern;
 
-public class EiRequest {
+/*
+TODO
+There are different types of requests!
+- "main" gives a menu, a prompt, asks for a one character response, and routes to another
+    menu based on that
+- "add-student" gives a prompt, asks for a longer response, and takes some action (storing
+    the name) before routing to parent
+- "list-students" only provides some information and does not ask for anything before
+    routing to parent
+
+INHERITAAANCE
+ */
+
+class EiRequest {
 
     private final String requestHead;
     private final String prompt;
     private final EiMenu menu;
     private final HashMap<String, String> forward;
+    private final String parent;
     private final Pattern pattern;
 
     public EiRequest(
@@ -14,26 +28,47 @@ public class EiRequest {
             String prompt,
             EiMenu menu,
             HashMap<String, String> forward,
+            String parent,
             Pattern pattern) {
         this.requestHead = requestHead;
         this.prompt = prompt;
         this.menu = menu;
         this.forward = forward;
+        this.parent = parent;
         this.pattern = pattern;
     }
 
     public EiRequest(
             String requestHead,
             String prompt,
+            String parent,
             Pattern pattern) {
         this.requestHead = requestHead;
         this.prompt = prompt;
         this.menu = new EiMenu();
         this.forward = new HashMap<>();
+        this.parent = parent;
         this.pattern = pattern;
     }
+
+    public EiRequest(
+            String requestHead,
+            String parent
+            ) {
+        this.requestHead = requestHead;
+        this.prompt = "";
+        this.menu = new EiMenu();
+        this.forward = new HashMap<>();
+        this.parent = parent;
+        this.pattern = Pattern.compile("");
+    }
+
     public boolean hasMenu() {
         return !menu.isEmpty();
+    }
+
+    public boolean hasPrompt() {
+        return !prompt.isEmpty();
     }
 
     public Pattern getPattern() {
@@ -54,6 +89,10 @@ public class EiRequest {
 
     public HashMap<String, String> getForward() {
         return forward;
+    }
+
+    public String getParent() {
+        return parent;
     }
 
     public String toString() {
