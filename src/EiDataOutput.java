2923eb14b7553aa9f603226bb428f92fed86ef8f
Vince <vince@krijnsen.com>
2023-10-20 12:31:55 +0100

    Refactor to inherit from EiRequest

--- src/EiDataOutput.java
+++ src/EiDataOutput.java
@@ -1,10 +1,14 @@
 class EiDataOutput extends EiRequest {
-    public EiDataOutput(
-            String requestHead,
-            String parent
-    ) {
+    public EiDataOutput(String requestHead, EiRequest parent) {
         this.requestHead = requestHead;
         this.parent = parent;
     }
 
+    public void transmit() {
+        System.out.println(requestHead);
+    }
+
+    public EiRequest getNext() {
+        return parent;
+    }
 }
