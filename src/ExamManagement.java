b8ec57b4b33a67c4e970580be1147c2fc4b848ba
Vince <vince@krijnsen.com>
2023-10-23 06:16:44 +0100

    fix: handle ExamException

--- src/ExamManagement.java
+++ src/ExamManagement.java
@@ -1,15 +1,22 @@
-import system.Essay;
+import system.exam.Essay;
+import system.exam.ExamException;
 
 class ExamManagement {
+    Essay testOne;
     public ExamManagement() {
-        Essay testOne = new Essay(
-                "Software dev",
-                90,
-                "What is it all about, when you get right down to it?",
-                96,
-                94,
-                12
-        );
+        try {
+            testOne = new Essay(
+                    4,
+                    "Software dev",
+                    90,
+                    "What is it all about, when you get right down to it?",
+                    96,
+                    94,
+                    12
+            );
+        } catch (ExamException ee) {
+            System.out.println("Error: " + ee.getMessage());
+        }
         System.out.println(testOne.displayExamDetails());
     }
 }
