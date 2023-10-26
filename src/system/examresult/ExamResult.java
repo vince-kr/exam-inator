5c18f1a0417c4d6c052d18b4f35a38d7717b2577
Vince <vince@krijnsen.com>
2023-10-26 17:01:57 +0100

    add: implement compareTo in ExamResult

--- src/system/examresult/ExamResult.java
+++ src/system/examresult/ExamResult.java
@@ -3,7 +3,7 @@ package system.examresult;
 import system.exam.Exam;
 import system.student.Student;
 
-public class ExamResult {
+public class ExamResult implements Comparable {
     Student student;
     Exam exam;
     int score;
@@ -12,6 +12,15 @@ public class ExamResult {
         this.student = student;
         this.exam = exam;
         this.score = score;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (!(o instanceof ExamResult)) {
+            return 0;
+        }
+        ExamResult other = (ExamResult) o;
+        return this.score - other.score;
     }
 
     public String toString() {
